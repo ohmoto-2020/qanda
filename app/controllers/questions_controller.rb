b8772f9b1ca948b463623e9db56f6a60b5d5ecaa
ohmoto <vitamin29290209@gmail.com>
2022-09-06 21:49:26 +0900

    crud

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,21 +19,34 @@ class QuestionsController < ApplicationController
     # Questionモデルを初期化
     @question = Question.new(question_params)
     # QuestionモデルをDBへ保存
-    @question.save
-    # showへリダイレクト
-    redirect_to @question
+    if @question.save
+      # showへリダイレクト
+      redirect_to @question
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   # 質問編集
   def edit
+    @question = Question.find(params[:id])
   end
 
   # 質問更新
   def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   # 質問削除
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
   private

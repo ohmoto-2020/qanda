e18c2538be65fa7fc831e1b5881fa318f98a7275
ohmoto <vitamin29290209@gmail.com>
2022-09-06 19:57:36 +0900

    add

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,27 @@
 class QuestionsController < ApplicationController
   # 質問一覧表示
   def index
+    @questions = Question.all
   end
 
   # 質問詳細表示
   def show
+    @question = Question.find(params[:id])
   end
 
   # 質問作成
   def new
+    @question = Question.new
   end
 
   # 質問登録
   def create
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBへ保存
+    @question.save
+    # showへリダイレクト
+    redirect_to @question
   end
 
   # 質問編集
@@ -25,5 +34,10 @@ class QuestionsController < ApplicationController
 
   # 質問削除
   def destroy
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title,:name,:content)
   end
 end
